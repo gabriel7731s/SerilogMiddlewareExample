8c3640d5e602a405d28d15ddea2e3736b71730c4
gabriel7731s <gabrielsilva7731@gmail.com>
2020-03-16 10:17:39 -0300

    initial commit

---
+++ MiddlewareExample/SerilogMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareExample
+{
+    class SerilogMiddleware
+    {
+        const string MessageTemplate = "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+
+        readonly RequestDelegate _next;
+
+        public SerilogMiddleware(RequestDelegate next)
+        {
+            if (next == null) throw new ArgumentNullException(nameof(next));
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext == null) throw new ArgumentNullException(nameof(httpContext));
+
+            var start = Stopwatch.GetTimestamp();
+
+            await _next(httpContext);
+
+            var elapsedMs = GetElapsedMilliseconds(start, Stopwatch.GetTimestamp());
+
+            var requestMethod = httpContext.Request.Method;
+
+            var requestPath = GetPath(httpContext);
+
+            var statusCode = httpContext.Response?.StatusCode;
+
+            var level = statusCode > 499 ? LogEventLevel.Error : LogEventLevel.Information;
+
+            Log.Write(level, MessageTemplate, requestMethod, requestPath, statusCode, elapsedMs);
+        }
+
+        static double GetElapsedMilliseconds(long start, long stop)
+        {
+            return (stop - start) * 1000 / (double)Stopwatch.Frequency;
+        }
+
+        static string GetPath(HttpContext httpContext)
+        {
+            return httpContext.Features.Get<IHttpRequestFeature>()?.RawTarget ?? httpContext.Request.Path.ToString();
+        }
+    }
+}
